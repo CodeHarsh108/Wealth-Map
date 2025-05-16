c13682eea2717567be03f74eff5e71aba70db379
Harsh Patil <itsharshhh6@gmail.com>
2025-05-16 08:32:08 +0530

    git commit -m"New Project"

---
+++ src/main/java/com/wealthmap/wealthmap_backend/WealthmapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wealthmap.wealthmap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthmapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthmapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wealthmap/wealthmap_backend/WealthmapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wealthmap.wealthmap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthmapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
