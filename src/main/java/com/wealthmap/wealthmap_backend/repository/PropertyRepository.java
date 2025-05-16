dafc5e95d5a4713535c9ade583680383f914bb3f
Harsh Patil <itsharshhh6@gmail.com>
2025-05-16 11:07:38 +0530

    git commit -m"Created Repo class"

---
+++ src/main/java/com/wealthmap/wealthmap_backend/repository/PropertyRepository.java
@@ -0,0 +1,9 @@
+package com.wealthmap.wealthmap_backend.repository;
+
+import com.wealthmap.wealthmap_backend.model.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+}
