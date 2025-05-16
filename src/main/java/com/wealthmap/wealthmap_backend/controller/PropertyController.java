e562f4030bd786c0a26137b362c0596bff8169af
Harsh Patil <itsharshhh6@gmail.com>
2025-05-16 11:10:30 +0530

    git commit -m"Initiated controller class"

---
+++ src/main/java/com/wealthmap/wealthmap_backend/controller/PropertyController.java
@@ -0,0 +1,9 @@
+package com.wealthmap.wealthmap_backend.controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class PropertyController {
+}
